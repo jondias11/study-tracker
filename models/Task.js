190064b5b2d02f9c6c4de992f55bb5a87bf090d5
jondias11 <johndias0702@gmail.com>
2026-04-21 20:44:28 +0530

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema({
-  date: String,
   title: String,
-  category: String,
   duration: Number,
-  completed: { type: Boolean, default: false }
+  completed: Boolean,
+  date: String,
+  category: String,
 });
 
 export default mongoose.models.Task || mongoose.model("Task", TaskSchema);
